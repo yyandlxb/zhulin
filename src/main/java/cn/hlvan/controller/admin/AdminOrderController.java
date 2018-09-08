14b0e9d789c9a362139d5b5c41fc76734df65529
yaoyuan <yaoyuan@hlvan.cn>
2018-09-08 20:34:24 +0800

    add user info

--- src/main/java/cn/hlvan/controller/admin/AdminOrderController.java
+++ src/main/java/cn/hlvan/controller/admin/AdminOrderController.java
@@ -2,14 +2,24 @@ package cn.hlvan.controller.admin;
 
 import cn.hlvan.configure.RequestJson;
 import cn.hlvan.form.AuditingForm;
+import cn.hlvan.manager.database.tables.records.OrderEssayRecord;
+import cn.hlvan.manager.database.tables.records.OrderRecord;
+import cn.hlvan.security.AuthorizedUser;
+import cn.hlvan.security.session.Authenticated;
 import cn.hlvan.service.OrderService;
 import cn.hlvan.util.Reply;
+import cn.hlvan.view.MerchantOrderDetail;
+import org.jooq.Condition;
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static cn.hlvan.manager.database.tables.Order.ORDER;
+import static cn.hlvan.manager.database.tables.OrderEssay.ORDER_ESSAY;
+import static cn.hlvan.manager.database.tables.UserOrder.USER_ORDER;
 
 @RestController("adminOrderController")
 @RequestMapping("/admin/merchant/order")
@@ -45,5 +55,20 @@ public class AdminOrderController {
         }else {
             return Reply.fail().message("分配失败");
         }
+    }
+    @GetMapping("/detail")
+    public Reply list(@RequestParam Integer id, @Authenticated AuthorizedUser user) {
+        MerchantOrderDetail merchantOrderDetail = new MerchantOrderDetail();
+        OrderRecord orderRecord = dsl.selectFrom(ORDER).where(ORDER.ID.eq(id)).fetchSingleInto(OrderRecord.class);
+        merchantOrderDetail.setOrderRecord(orderRecord);
+        List<OrderEssayRecord> orderEssayRecords = dsl.select(ORDER_ESSAY.fields())
+                                                      .from(USER_ORDER)
+                                                      .innerJoin(ORDER_ESSAY)
+                                                      .on(ORDER_ESSAY.USER_ORDER_ID.eq(USER_ORDER.ID))
+                                                      .where(USER_ORDER.ORDER_CODE.eq(orderRecord.getOrderCode()))
+                                                      .orderBy(USER_ORDER.UPDATED_AT.desc())
+                                                      .fetchInto(OrderEssayRecord.class);
+        merchantOrderDetail.setOrderEssayRecords(orderEssayRecords);
+        return Reply.success().data(merchantOrderDetail);
     }
 }
