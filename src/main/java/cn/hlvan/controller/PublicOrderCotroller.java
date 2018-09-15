a43a1481cc1450d84e5b2a2f419891cb605d45ff
yaoyuan <yaoyuan@hlvan.cn>
2018-09-15 22:02:04 +0800

    add user info

--- src/main/java/cn/hlvan/controller/PublicOrderCotroller.java
+++ src/main/java/cn/hlvan/controller/PublicOrderCotroller.java
@@ -13,10 +13,16 @@ import cn.hlvan.util.Reply;
 import cn.hlvan.view.MerchantOrderDetail;
 import org.jooq.Condition;
 import org.jooq.DSLContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -30,8 +36,11 @@ import static cn.hlvan.manager.database.tables.UserOrder.USER_ORDER;
 @RestController("publicOrderController")
 @RequestMapping("/merchant/order")
 public class PublicOrderCotroller {
-    private DSLContext dsl;
 
+    @Value("${file.path}")
+    private String path;
+    private DSLContext dsl;
+    private static Logger logger = LoggerFactory.getLogger(PublicOrderCotroller.class);
     @Autowired
     public void setDsl(DSLContext dsl) {
         this.dsl = dsl;
@@ -114,5 +123,56 @@ public class PublicOrderCotroller {
         }
 
     }
+    @GetMapping("/download")
+    public Reply downloadFile(HttpServletResponse response,String fileName) throws UnsupportedEncodingException {
 
+        // 如果文件名不为空，则进行下载
+        if (fileName != null) {
+            //设置文件路径
+            String realPath = path;
+            File file = new File(realPath, fileName);
+            // 如果文件名存在，则进行下载
+            if (file.exists()) {
+                // 配置文件下载
+                response.setHeader("content-type", "application/octet-stream");
+                response.setContentType("application/octet-stream");
+                // 下载文件能正常显示中文
+                response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+                // 实现文件下载
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                }
+                catch (Exception e) {
+                    logger.info("Download the song failed!",e);
+                }
+                finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
